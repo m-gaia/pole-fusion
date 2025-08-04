558830d3f349377bfdd79ccc79150f89243de2b0
Marianela Gaia <mgaia@triunfoseguros.com>
2025-08-04 20:40:38 -0300

    Agregar padding-top para evitar superposición con navbar

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -4,7 +4,7 @@ import MembershipSystem from '../components/MembershipSystem'
 
 const Bookings = () => {
   return (
-    <div className="min-h-screen bg-gray-50 py-12">
+    <div className="min-h-screen bg-gray-50 py-12 pt-32">
       <div className="container mx-auto px-4">
         {/* Hero Section */}
         <motion.div
